c77713fb6367c14765ac593c7e1224c329010da9
lucas-catto <lucascattosilva2024@gmail.com>
2024-12-26 21:57:47 -0300

    Create a MySQL Database

---
+++ 01 - Dynamic Web Applications/16 - Create a MySQL Database/database.sql
@@ -0,0 +1,16 @@
+
+DROP DATABASE IF EXISTS myapp;
+CREATE DATABASE IF NOT EXISTS myapp;
+USE myapp;
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL
+);
+
+INSERT INTO posts (title) VALUES
+('My first post'),
+('My second post'),
+('My third post');
+
+SELECT * FROM posts;
